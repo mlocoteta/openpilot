213ca031e1967c624f4f50a6aaadfd5aa3f6a973
Mathew Locoteta <m.locoteta@gmail.com>
2023-03-17 18:00:53 -0400

    added torque blending and fixed temp_unavailable

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -5,7 +5,7 @@ from selfdrive.controls.lib.drive_helpers import rate_limit
 from common.numpy_fast import clip, interp
 from selfdrive.car import create_gas_command
 from selfdrive.car.honda import hondacan
-from selfdrive.car.honda.values import OLD_NIDEC_LONG_CONTROL, CruiseButtons, CAR, VISUAL_HUD, HONDA_BOSCH, CarControllerParams, SERIAL_STEERING
+from selfdrive.car.honda.values import OLD_NIDEC_LONG_CONTROL, CruiseButtons, CAR, VISUAL_HUD, HONDA_BOSCH, CarControllerParams, SERIAL_STEERING, LKAS_LIMITS
 from opendbc.can.packer import CANPacker
 from selfdrive.car import apply_std_steer_torque_limits, apply_serial_steering_torque_mod
 
@@ -135,25 +135,21 @@ class CarController():
     # **** process the car messages ****
 
     # steer torque is converted back to CAN reference (positive when steering right)
-    apply_steer = int(interp(actuators.steer * P.STEER_MAX, P.STEER_LOOKUP_BP, P.STEER_LOOKUP_V))
+    apply_steer = int(interp(actuators.steer * self.params.STEER_MAX,
+                             self.params.STEER_LOOKUP_BP, self.params.STEER_LOOKUP_V))
 
-    if (CS.CP.carFingerprint in SERIAL_STEERING): # Dynamic torque boost if above threshold, smooth torque blend otherwise
-      if (apply_steer >= self.steer_torque_boost_min) or (apply_steer <= -self.steer_torque_boost_min):
-        apply_steer = apply_serial_steering_torque_mod(apply_steer, self.steer_torque_boost_min, self.apply_steer_warning_counter, self.apply_steer_cooldown_counter)
-      else:
-        apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, self.params)
-        self.apply_steer_warning_counter = 0
-        self.apply_steer_cooldown_counter = 0
+    if (CS.CP.carFingerprint in SERIAL_STEERING):
+      apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, LKAS_LIMITS, ss=True)
       self.apply_steer_last = apply_steer
-
-    # Add low steering torque cap for low speeds, manual turning, & manual interventions
-    if (CS.steer_torque_limited and (apply_steer > 60 or apply_steer < -60)):
-      apply_steer = 60 if apply_steer > 0 else -60
 
     # steer torque is converted back to CAN reference (positive when steering right)
     apply_steer = -apply_steer
     
-    # Send CAN commands.
+    #stops steer temp unavailable
+    if apply_steer == 0:  
+      lkas_active = False
+
+    # Send CAN commands
     can_sends = []
 
     # tester present - w/ no response (keeps radar disabled)
